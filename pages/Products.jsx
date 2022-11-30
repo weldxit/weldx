91dd36cebe358027b521338bed8b2af7c2a91a32
weldxit <subhransu.weldxit@gmail.com>
2022-11-30 18:39:51 +0530

    product page added

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import style from "../styles/Product.module.css";
 import Default from "./Default";
+import Footer from './components/Footer'
 export default function Products() {
   return (
     <div className={style.root}>
@@ -14,24 +15,221 @@ export default function Products() {
               <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
               <div className={style.erp_features}>
                 <ul className={style.feature_list}>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu </li>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu</li>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu</li>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu</li>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu</li>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu</li>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu</li>
-                  <li>lorem ipsum dolar emet set mat mesu menatsu</li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
                 </ul>
               </div>
             </div>
-            <div className={style.erp_card}>my card</div>
-            <div className={style.erp_card}>my card</div>
-            <div className={style.erp_card}>my card</div>
-            <div className={style.erp_card}>my card</div>
+            <div className={style.erp_card}>
+              {" "}
+              <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
+              <div className={style.erp_features}>
+                <ul className={style.feature_list}>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={style.erp_card}>
+              {" "}
+              <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
+              <div className={style.erp_features}>
+                <ul className={style.feature_list}>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={style.erp_card}>
+              {" "}
+              <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
+              <div className={style.erp_features}>
+                <ul className={style.feature_list}>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={style.erp_card}>
+              {" "}
+              <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
+              <div className={style.erp_features}>
+                <ul className={style.feature_list}>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={style.erp_card}>
+              {" "}
+              <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
+              <div className={style.erp_features}>
+                <ul className={style.feature_list}>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                  <li className={style.li}>
+                    lorem ipsum dolar emet set mat mesu menatsu{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </section>
+
+      <section className={style.featured_product}>
+        <div className={style.customeErp}>
+          <h1 className={style.h1}>Customized ERP</h1>
+          <div className={style.making_erp}>
+            <h2 className={style.h2}>Give your problem, Take your solution.</h2>
+
+            <div className={style.chain}>
+              <div className={style.req}> <span className={style.text}> Your Requirements </span></div>
+              <div className={style.team}> <span className={style.text}>  Our Team </span></div>
+              <div className={style.sol}> <span className={style.text}> Your Solution </span></div>
+              <div className={style.you}> <span className={style.text}> Happy You </span></div>
+            </div>
+          </div>
+        </div>
+      </section >
+
+      <section className={style.section3}>
+        <div className={style.featuring_products}>
+              <h1>
+            
+              </h1>
+        </div>
+      </section>
+      <Footer/>
     </div>
   );
 }
--- pages/Services.jsx
+++ pages/Services.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import style from '../styles/Service.module.css'
 import Default from './Default'
+import Footer from './components/Footer'
 
 export default function Services() {
   return (
     <div className={style.section}>
         <Navbar/>
-        <section >
-            <div>
+        <section className={style.services} >
+            <div className={style.service_providing}>
                 
             </div>
         </section>
+        <Footer/>
     </div>
   )
 }
