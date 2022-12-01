bb2709692a6e41cc827da042e1c1f80911c17a56
weldxit <subhransu.weldxit@gmail.com>
2022-12-01 19:16:10 +0530

    product page

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import style from "../styles/Product.module.css";
 import Default from "./Default";
-import Footer from './components/Footer'
+import Footer from "./components/Footer";
+import Image from "next/image";
 export default function Products() {
   return (
     <div className={style.root}>
@@ -44,7 +45,7 @@ export default function Products() {
             </div>
             <div className={style.erp_card}>
               {" "}
-              <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
+              <h1 className={style.erp_name}>Medicash Smart Hospital <br /> ERP</h1>
               <div className={style.erp_features}>
                 <ul className={style.feature_list}>
                   <li className={style.li}>
@@ -76,7 +77,7 @@ export default function Products() {
             </div>
             <div className={style.erp_card}>
               {" "}
-              <h1 className={style.erp_name}>Educrush Smart School ERP</h1>
+              <h1 className={style.erp_name}>Inventory & Stock <br /> Management ERP</h1>
               <div className={style.erp_features}>
                 <ul className={style.feature_list}>
                   <li className={style.li}>
@@ -210,26 +211,47 @@ export default function Products() {
         <div className={style.customeErp}>
           <h1 className={style.h1}>Customized ERP</h1>
           <div className={style.making_erp}>
-            <h2 className={style.h2}>Give your problem, Take your solution.</h2>
+            <h2 className={style.h2}>Tell your problem, Take your solution.</h2>
 
             <div className={style.chain}>
-              <div className={style.req}> <span className={style.text}> Your Requirements </span></div>
-              <div className={style.team}> <span className={style.text}>  Our Team </span></div>
-              <div className={style.sol}> <span className={style.text}> Your Solution </span></div>
-              <div className={style.you}> <span className={style.text}> Happy You </span></div>
+              <div className={style.req}>
+                {" "}
+                <span className={style.text}> Your Requirements </span>
+              </div>
+              <div className={style.team}>
+                {" "}
+                <span className={style.text}> Our Team </span>
+              </div>
+              <div className={style.sol}>
+                {" "}
+                <span className={style.text}> Your Solution </span>
+              </div>
+              <div className={style.you}>
+                {" "}
+                <span className={style.text}> Happy You </span>
+              </div>
             </div>
           </div>
         </div>
-      </section >
+      </section>
 
       <section className={style.section3}>
         <div className={style.featuring_products}>
-              <h1>
-            
-              </h1>
+          <div className={style.details_iot}>
+            <h1 className={style.s4}>Smart Security & Survelliance System</h1>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quo
+            sit minus omnis. Earum rem dicta, atque dolores assumenda ipsum ad
+            repellat vero libero. Error fugiat culpa architecto praesentium
+            provident? Architecto cum unde suscipit sunt qui quibusdam quis
+            adipisci maiores nulla vero culpa, hic minus harum aut vitae
+            placeat, a optio est. Eligendi earum quaerat placeat quis, obcaecati
+          </div>
+          <div className={style.image_iot}>
+            <Image src={"/iot.png"} fill={true} alt="iot" />
+          </div>
         </div>
       </section>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
