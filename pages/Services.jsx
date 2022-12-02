f987436e66c79d8e230b99578ccbf067838bbd16
weldxit <subhransu.weldxit@gmail.com>
2022-12-02 12:42:52 +0530

    pushes

--- pages/Services.jsx
+++ pages/Services.jsx
@@ -1,19 +1,103 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import style from '../styles/Service.module.css'
-import Default from './Default'
-import Footer from './components/Footer'
+import React from "react";
+import Navbar from "./components/Navbar";
+import style from "../styles/Service.module.css";
+import Default from "./Default";
+import Footer from "./components/Footer";
+import Image from "next/image";
 
 export default function Services() {
   return (
     <div className={style.section}>
-        <Navbar/>
-        <section className={style.services} >
-            <div className={style.service_providing}>
-                
-            </div>
+      <Navbar />
+      <section className={style.services}>
+        <section>
+        <div className={style.each_service}>
+          <div className={style.service_text}>
+
+          <h1 className={style.each_service_name}>Web Developement</h1>
+          <p className={style.each_service_para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            recusandae at error magni assumenda nulla accusantium fugit quam
+            dolorum minus illum sequi beatae, delectus rem quisquam, nisi
+            cupiditate eaque explicabo.
+          </p>
+          </div>
+
+          <div className={style.service_img}>
+                <Image src={'/trust-love.PNG'} fill={true} alt='img'/>
+          </div>
+        </div>
         </section>
-        <Footer/>
+
+        <div className={style.each_service}>
+          <div className={style.service_text}>
+
+          <h1 className={style.each_service_name}>Web Developement</h1>
+          <p className={style.each_service_para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            recusandae at error magni assumenda nulla accusantium fugit quam
+            dolorum minus illum sequi beatae, delectus rem quisquam, nisi
+            cupiditate eaque explicabo.
+          </p>
+          </div>
+
+          <div className={style.service_img}>
+                <Image src={'/trust-love.PNG'} fill={true} alt='img'/>
+          </div>
+        </div>
+
+        <div className={style.each_service}>
+          <div className={style.service_text}>
+
+          <h1 className={style.each_service_name}>Web Developement</h1>
+          <p className={style.each_service_para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            recusandae at error magni assumenda nulla accusantium fugit quam
+            dolorum minus illum sequi beatae, delectus rem quisquam, nisi
+            cupiditate eaque explicabo.
+          </p>
+          </div>
+
+          <div className={style.service_img}>
+                <Image src={'/trust-love.PNG'} fill={true} alt='img'/>
+          </div>
+        </div>
+
+        <div className={style.each_service}>
+          <div className={style.service_text}>
+
+          <h1 className={style.each_service_name}>Web Developement</h1>
+          <p className={style.each_service_para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            recusandae at error magni assumenda nulla accusantium fugit quam
+            dolorum minus illum sequi beatae, delectus rem quisquam, nisi
+            cupiditate eaque explicabo.
+          </p>
+          </div>
+
+          <div className={style.service_img}>
+                <Image src={'/trust-love.PNG'} fill={true} alt='img'/>
+          </div>
+        </div>
+
+        <div className={style.each_service}>
+          <div className={style.service_text}>
+
+          <h1 className={style.each_service_name}>Web Developement</h1>
+          <p className={style.each_service_para}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            recusandae at error magni assumenda nulla accusantium fugit quam
+            dolorum minus illum sequi beatae, delectus rem quisquam, nisi
+            cupiditate eaque explicabo.
+          </p>
+          </div>
+
+          <div className={style.service_img}>
+                <Image src={'/trust-love.PNG'} fill={true} alt='img'/>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
-  )
+  );
 }
